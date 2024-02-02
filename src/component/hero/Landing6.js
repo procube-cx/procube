9ec431f7d71d55c2a592a6e42db40e6c0e785c79
dhanush <dhanushpk50@gmail.com>
2024-02-03 00:13:34 +0530

    changes

--- src/component/hero/Landing6.js
+++ src/component/hero/Landing6.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import process from '../../assets/images/approach.png';
+import { motion } from 'framer-motion';
 
 const data = [
     {
@@ -30,18 +31,29 @@ const data = [
 
 ]
 
-const Card = ({ id, title, description, activeCard, onCardClick }) => {
+const Card = ({ id, title, description, activeCard, onCardClick, pauseTimer, resumeTimer }) => {
     const [isOpen, setIsOpen] = useState(activeCard === id);
-    if (activeCard === id && !isOpen) {
-        setIsOpen(true);
-    }
-    else if (activeCard !== id && isOpen) {
-        setIsOpen(false);
-    }
+    const [isHovered, setIsHovered] = useState(false);
+
+    useEffect(() => {
+        setIsOpen(activeCard === id);
+    }, [activeCard, id]);
+
+    const handleHover = () => {
+        setIsHovered(true);
+        pauseTimer();
+    };
+
+    const handleHoverExit = () => {
+        setIsHovered(false);
+        resumeTimer();
+    };
 
     return (
         <div
             className={`flex flex-col w-full border-b-2 border-opacity-40 border-white py-3 gap-2 ${!isOpen && 'opacity-40'}`}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHoverExit}
             onClick={() => onCardClick(id)}
         >
             <div className='grid grid-cols-6 flex-1'>
@@ -53,45 +65,66 @@ const Card = ({ id, title, description, activeCard, onCardClick }) => {
                     <p className='col-span-5 col-start-2 '>{description}</p>
                 </div>
             }
+            {isOpen &&
+                <motion.div
+                    style={{ scaleX: activeCard ? 1 : 0, transformOrigin: "left" }}
+                    className='w-full h-1 bg-white'
+                    initial={{ scaleX: 0 }}
+                    animate={{ scaleX: isHovered ? 0 : activeCard ? 1 : 0 }}
+                    transition={{ duration: 6 }}
+                />
+            }
         </div>
     );
 };
 
 const Landing6 = () => {
     const [activeCard, setActiveCard] = useState(1);
+    const [timerPaused, setTimerPaused] = useState(false);
 
     const handleCardClick = (id) => {
-        setActiveCard((prevActiveCard) => (prevActiveCard === id ? null : id));
+        setActiveCard(id);
+    };
+
+    const pauseTimer = () => {
+        setTimerPaused(true);
+    };
+
+    const resumeTimer = () => {
+        setTimerPaused(false);
     };
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            setActiveCard((prevActiveCard) => (prevActiveCard === 5 ? 1 : prevActiveCard + 1));
-        }, 5000);
-        console.log('activeCard', activeCard);
+            if (!timerPaused) {
+                setActiveCard((prevActiveCard) => (prevActiveCard === 5 ? 1 : prevActiveCard + 1));
+            }
+        }, 5000)
         return () => clearTimeout(timer);
-    }, [activeCard]);
+    }, [activeCard, timerPaused]);
 
     return (
         <div className='w-full px-6 md:px-24 py-6 md:py-10  min-h-screen max-h-[900px]'>
             <p className='w-1/2 font-normal text-8xl !leading-tight uppercase'>Process & Approach</p>
-        <div className='w-full flex flex-col md:flex-row overflow-hidden gap-x-28'>
-            <div className='flex md:flex-1 h-full flex-col my-auto'>
-                {data.map((cardDetail) => (
-                    <Card
-                        key={cardDetail.id} // added key prop
-                        id={cardDetail.id}
-                        title={cardDetail.title}
-                        description={cardDetail.description}
-                        activeCard={activeCard}
-                        onCardClick={handleCardClick}
-                    />
-                ))}
+            <div className='w-full flex flex-col md:flex-row overflow-hidden gap-x-28'>
+                <div className='flex md:flex-1 h-full flex-col my-auto'>
+                    {data.map((cardDetail) => (
+                        <Card
+                            key={cardDetail.id} // added key prop
+                            id={cardDetail.id}
+                            title={cardDetail.title}
+                            description={cardDetail.description}
+                            activeCard={activeCard}
+                            onCardClick={handleCardClick}
+                            pauseTimer={pauseTimer}
+                            resumeTimer={resumeTimer}
+                        />
+                    ))}
+                </div>
+                <div className='flex md:flex-1 h-full flex-col justify-center'>
+                    <img src={process} alt="process" className='w-full mx-auto' />
+                </div>
             </div>
-            <div className='flex md:flex-1 h-full flex-col justify-center'>
-                <img src={process} alt="process" className='w-full mx-auto' />
-            </div>
-        </div>
         </div>
     );
 };
