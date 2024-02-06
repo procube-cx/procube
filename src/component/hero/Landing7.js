65ef367857ed8754e6cce1689a00e1e863102a09
dhanush <dhanushpk50@gmail.com>
2024-02-06 15:15:40 +0530

    cant do

--- src/component/hero/Landing7.js
+++ src/component/hero/Landing7.js
@@ -41,14 +41,7 @@ const Landing7 = () => {
 
     return (
         <div className='bg-vector bg-no-repeat px-6 md:px-24  h-screen relative overflow-hidden'>
-            <AnimatePresence initial={false} mode="wait">
-                <motion.div
-                    key={currentItem}
-                    initial={direction ? { opacity: 0, x: '-100%' } : { opacity: 0, x: '100%' }}
-                    animate={direction ? { opacity: 1, x: 0 } : { opacity: 1, x: 0 }}
-                    // exit={direction ? { opacity: 0, x: '100%' } : { opacity: 0, x: '-100%' }}
-                    transition={{ duration: 0.5 }}
-                    className='flex flex-col h-full justify-center'>
+                <div className='flex flex-col h-full justify-center'>
 
                     <div className='flex flex-col gap-12'>
                         <p className=' text-4xl md:text-8xl italic font-normal'>“{data[currentItem].heading}”</p>
@@ -58,22 +51,7 @@ const Landing7 = () => {
                             <p className='font-normal italic'>{data[currentItem].position}</p>
                         </div>
                     </div>
-                </motion.div>
-            </AnimatePresence>
-            <div className='absolute bottom-0 left-1/2 transform -translate-x-1/2 flex gap-4'>
-                <button
-                    className='px-4 py-2 bg-gray-800 text-white rounded-md focus:outline-none'
-                    onClick={prevItem}
-                >
-                    Previous
-                </button>
-                <button
-                    className='px-4 py-2 bg-gray-800 text-white rounded-md focus:outline-none'
-                    onClick={nextItem}
-                >
-                    Next
-                </button>
-            </div>
+                </div>
 
         </div>
     )
