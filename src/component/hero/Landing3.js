63dc4db13d71da2393adef6fb4a684c41749fe69
dhanush <dhanushpk50@gmail.com>
2024-02-06 17:22:35 +0530

    changes

--- src/component/hero/Landing3.js
+++ src/component/hero/Landing3.js
@@ -28,7 +28,6 @@ const Landing3 = () => {
       <p className='font-medium text-2xl md:text-5xl leading-10 md:!leading-[78px] text-center'>
       <span className='text'>At Procube, we unlock innovation's power. Collaborating with brands and visionaries, we revitalize identities, craft impactful digital journeys, and lead transformative advancements. Welcome to Procube, where innovation fuels growth, redefines brands, and turns dreams into reality
       </span>
-
       </p>
     </div>
   );
