24280ad74896c021facab90b26b86872127392f0
dhanush <dhanushpk50@gmail.com>
2024-02-06 15:54:00 +0530

    carousel finished

--- src/component/hero/Landing7.js
+++ src/component/hero/Landing7.js
@@ -1,5 +1,27 @@
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import './hero.css';
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
 
 const data = [
@@ -40,20 +62,34 @@ const Landing7 = () => {
 
 
     return (
-        <div className='bg-vector bg-no-repeat px-6 md:px-24  h-screen relative overflow-hidden'>
-                <div className='flex flex-col h-full justify-center'>
+        <Carousel 
+        responsive={responsive}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={5000}
+        
+        transitionDuration={500}
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        // customRightArrow={}        
+        >
+            {data.map((item, index) => (
+                <div className='bg-vector bg-no-repeat px-6 md:px-24  h-screen relative overflow-hidden' >
+                    <div className='flex flex-col h-full justify-center'>
 
-                    <div className='flex flex-col gap-12'>
-                        <p className=' text-4xl md:text-8xl italic font-normal'>“{data[currentItem].heading}”</p>
-                        <p className=' text-xl md:text-4xl opacity-50 font-normal'>{data[currentItem].details}</p>
-                        <div className='flex flex-col gap-2 text-base md:text-3xl'>
-                            <p className=' font-medium '>{data[currentItem].name}</p>
-                            <p className='font-normal italic'>{data[currentItem].position}</p>
+                        <div className='flex flex-col gap-12'>
+                            <p className=' text-4xl md:text-8xl italic font-normal'>“{data[index].heading}”</p>
+                            <p className=' text-xl md:text-4xl opacity-50 font-normal'>{data[index].details}</p>
+                            <div className='flex flex-col gap-2 text-base md:text-3xl'>
+                                <p className=' font-medium '>{data[index].name}</p>
+                                <p className='font-normal italic'>{data[index].position}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-        </div>
+
+                </div>
+            ))}
+        </Carousel>
     )
 }
 
