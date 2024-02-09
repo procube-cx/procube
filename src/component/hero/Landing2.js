ae0154a0055cf0b2e2e9d9e965302af9a7de1025
dhanush <dhanushpk50@gmail.com>
2024-02-09 20:30:10 +0530

    changes

--- src/component/hero/Landing2.js
+++ src/component/hero/Landing2.js
@@ -5,54 +5,38 @@ import large from '../../assets/images/large.png';
 // import video from '../../assets/video/service-1.webm'
 
 const Landing2 = () => {
+
   useEffect(() => {
+
+    const screenheight = window.innerHeight;
+    const screenwidth = window.innerWidth;
     gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#img',
+        start: '-40% center',
+        end: 'bottom center',
+        scrub: 1,
+      },
+    });
+    tl.set('.image-wrapper #img', {
+      scale: 0.2,
+      x: screenwidth/8,
+      y: -screenheight/2,
+      duration: 2,
+      ease: 'power4.out',
+      transformOrigin: 'center top',
 
-    const tl = gsap.timeline();
-    const halfScreenHeight =  window.innerHeight ;
-
-    // Initial animation settings to move the image
-tl.from(".image-wrapper #img", {
-  x: "20%", // Move 20% to the right
-  y: -halfScreenHeight, // Move up by half of the screen height
-  scale: 0.15, // Scale down to 15%
-  transformOrigin: "  center center", // Set the transformation origin to the top center
-  ease: "power4.out", // Easing function
-});
-
-// Resetting the image position and scale
-tl.to(".image-wrapper #img", {
-  x: 0, // Reset x position
-  y: 0, // Reset y position
-  scale: 1, // Reset scale
-  transformOrigin: "  center center", // Change transformation origin
-  ease: "power4.out", // Easing function
-});
-
-// Another animation setting, which seems redundant
-tl.to(".image-wrapper #img", {
-  x: 0,
-  y: 0,
-  scale: 1,
-  transformOrigin: "  center center",
-  ease: "power4.out",
-});
-
-// Creating ScrollTrigger to control the animation
-ScrollTrigger.create({
-  trigger: ".cont", // The trigger element
-  start: "top center", // When to start the animation
-  end: "bottom center", // When to end the animation
-  animation: tl, // Animation to be triggered
-  scrub: true, // Whether to smoothly animate through the scroll
-  markers: false // Whether to display markers for debugging
-});
-
-
-    return () => {
-      // Clean up function if needed
-    };
+    });
+    tl.to('.image-wrapper  #img', {
+      scale: 1,
+      x: 0,
+      y: 0,
+      duration: 2,
+      ease: 'power4.out',
+    });
   }, []);
+
 
   return (
     <div className="cont">
