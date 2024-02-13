f6fc89f8b6534136d6b6cae53473fddbe117fa0c
dhanush <dhanushpk50@gmail.com>
2024-02-13 17:47:50 +0530

    changess

--- src/component/hero/Landing3.js
+++ src/component/hero/Landing3.js
@@ -25,7 +25,7 @@ const Landing3 = () => {
   }, []); // Close the useEffect dependencies array
 
   return (
-    <div className='px-6 md:px-24 py-10 md:py-24 container'>
+    <div className='px-6 md:px-24 py-10 md:py-24 '>
       <p className='font-medium text-2xl md:text-5xl leading-10 md:!leading-[78px] text-center'>
       <span className='text'>At Procube, we unlock innovation's power. Collaborating with brands and visionaries, we revitalize identities, craft impactful digital journeys, and lead transformative advancements. Welcome to Procube, where innovation fuels growth, redefines brands, and turns dreams into reality
       </span>
