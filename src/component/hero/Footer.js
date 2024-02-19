2eae04dc4a5bc388f2859f86f045f0b8f624a16c
dhanush <dhanushpk50@gmail.com>
2024-02-19 19:50:45 +0530

    changes

--- src/component/hero/Footer.js
+++ src/component/hero/Footer.js
@@ -87,7 +87,7 @@ const Footer = () => {
                 <div className='button bg-[#7605C1] px-12 md:px-16 py-3 md:py-4 text-base md:text-4xl rounded-full cursor-pointer'>Get Started</div>
             </div>
             <div className='flex flex-col md:flex-row mb-10  md:mb-20'>
-                <div className=' border px-5 py-10 md:px-7 md:py-16 rounded-full h-auto md:h-44 w-16 md:w-auto my-8 md:my-0 mr-16 md:mr-10 cursor-pointer'>
+                <div className=' border px-5 py-10 md:px-7 md:py-16 rounded-full h-auto md:h-44 w-16 md:w-auto my-8 md:my-0 mr-16 md:mr-10 cursor-pointer' onClick={() => window.scrollTo({top:0,behavior:'smooth'})}>
                     <img src={arrow} alt='arrow' className='w-6 md:w-8  ' />
                 </div>
                 <div className='flex-1 my-auto'>
