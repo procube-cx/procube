bef84427068447dc1826240fb53d7d71c1199821
dhanush <dhanushpk50@gmail.com>
2024-03-05 10:26:26 +0530

    error fixed testing

--- src/component/hero/Landing2.js
+++ src/component/hero/Landing2.js
@@ -38,72 +38,75 @@ const Landing2 = () => {
 
   // }, [ScrollTrigger]);
 
-  // useLayoutEffect(() => {
-  //   let ctx = gsap.context(() => {
-  //       const screenheight = window.innerHeight;
-  //       const screenwidth = window.innerWidth;
-  //       gsap.registerPlugin(ScrollTrigger);
-  //       const tl = gsap.timeline()
-  //       const scroll2 = ScrollTrigger.create({
-  //         trigger: '.cont',
-  //         start: '-150% center',
-  //         end: 'bottom center',
-  //         animation: tl,
-  //         scrub: 1,
-  //         markers: false
-  //       });
-  //       tl.from('.image-wrapper #img', {
-  //         scale: 0.14,
-  //         x: screenwidth / 8,
-  //         y: -screenheight / 2 + 30,
-  //         duration: 2,
-  //         ease: 'linear',
-  //         transformOrigin: 'center top',
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+        const screenheight = window.innerHeight;
+        const screenwidth = window.innerWidth;
+        gsap.registerPlugin(ScrollTrigger);
+        const tl = gsap.timeline()
+        const scroll2 = ScrollTrigger.create({
+          trigger: '.cont',
+          start: '-150% center',
+          end: 'bottom center',
+          animation: tl,
+          scrub: 1,
+          markers: false
+        });
+        tl.from('.image-wrapper #img', {
+          scale: 0.14,
+          x: screenwidth / 8,
+          y: -screenheight / 2 + 30,
+          duration: 2,
+          ease: 'linear',
+          transformOrigin: 'center top',
 
-  //       });
-  //       tl.to('.image-wrapper  #img', {
-  //         scale: 1,
-  //         x: 0,
-  //         y: 0,
-  //         duration: 2,
-  //         ease: 'linear',
-  //       });
-  //     }
-  //   )
+        });
+        tl.to('.image-wrapper  #img', {
+          scale: 1,
+          x: 0,
+          y: 0,
+          duration: 2,
+          ease: 'linear',
+        });
+      }
+    )
+    return () => {
+      ctx.clear()
+    }
+  }, [ScrollTrigger, window.innerHeight, window.innerWidth, window.scrollY, window.scrollX, window.scroll, window.scrollBy, window.scrollTo])
+
+  // useEffect(() => {
+  //   const screenheight = window.innerHeight;
+  //   const screenwidth = window.innerWidth;
+  //   gsap.registerPlugin(ScrollTrigger);
+  //   const tl = gsap.timeline()
+  //   ScrollTrigger.create({
+  //       trigger: '.cont',
+  //       start: '-150% center',
+  //       end: 'bottom center',
+  //       animation: tl,
+  //       scrub: 1,
+  //       markers:false
+  //   });
+  //   tl.from('.image-wrapper #img', {
+  //     scale: 0.14,
+  //     x: screenwidth/8,
+  //     y: -screenheight/2 + 30,
+  //     duration: 2,
+  //     ease: 'linear',
+  //     transformOrigin: 'center top',
+  //   });
+  //   tl.to('.image-wrapper  #img', {
+  //     scale: 1,
+  //     x: 0,
+  //     y: 0,
+  //     duration: 2,
+  //     ease: 'linear',
+  //   });
   //   return () => {
-  //     ctx.clear()
+  //     ScrollTrigger.getAll().forEach(t => t.kill());
   //   }
-  // }, [ScrollTrigger, window.innerHeight, window.innerWidth, window.scrollY, window.scrollX, window.scroll, window.scrollBy, window.scrollTo])
-
-  useEffect(() => {
-    const screenheight = window.innerHeight;
-    const screenwidth = window.innerWidth;
-    gsap.registerPlugin(ScrollTrigger);
-    const tl = gsap.timeline()
-    ScrollTrigger.create({
-        trigger: '.cont',
-        start: '-150% center',
-        end: 'bottom center',
-        animation: tl,
-        scrub: 1,
-        markers:false
-    });
-    tl.from('.image-wrapper #img', {
-      scale: 0.14,
-      x: screenwidth/8,
-      y: -screenheight/2 + 30,
-      duration: 2,
-      ease: 'linear',
-      transformOrigin: 'center top',
-    });
-    tl.to('.image-wrapper  #img', {
-      scale: 1,
-      x: 0,
-      y: 0,
-      duration: 2,
-      ease: 'linear',
-    });
-  }, []);
+  // }, [ScrollTrigger]);
 
   return (
     <div className="cont">
