5f995256dc150d5d71a086e2c4dd7eba326ef21c
dhanush <dhanushpk50@gmail.com>
2024-03-05 10:22:18 +0530

    error fixed testing

--- src/component/hero/Landing2.js
+++ src/component/hero/Landing2.js
@@ -103,10 +103,7 @@ const Landing2 = () => {
       duration: 2,
       ease: 'linear',
     });
-    return () => {
-      ScrollTrigger.getAll().forEach(t => t.kill());
-    }
-  }, [ScrollTrigger]);
+  }, []);
 
   return (
     <div className="cont">
