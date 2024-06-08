5212754dd35f61e8c7bf1582b3ca8b09ed5cce05
dhanush <dhanushpk50@gmail.com>
2024-06-08 19:42:08 +0530

    changes

--- src/component/insight/Insightview.js
+++ src/component/insight/Insightview.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import hero from '../../assets/images/insight.png'
 
+const Blogstructure = ({ data }) => {
+  return (
+    <>
+      {data.type === 'heading' && <p className='font-normal text-2xl md:text-4xl max-w-full text-center uppercase mx-auto !leading-tight'>{data.content}</p>}
+      {data.type === 'subheading' && <p className='font-normal text-xl md:text-2xl max-w-full text-center uppercase mx-auto !leading-tight'>{data.content}</p>}
+      {data.type === 'content' && <p className='text-center font-normal w-full md:w-5/6 mx-auto text-xs md:text-lg opacity-60'>{data.content}</p>}
+      {data.type === 'ul' && <ul className='text-center font-normal w-full md:w-5/6 mx-auto text-xs md:text-lg opacity-60'>{data.ul.map((data, index) => (
+        <li key={index}>{data}</li>
+      ))}</ul>}
+    </>
+  )
+}
+
 
 const Insightview = (blogdata) => {
   const slug = window.location.pathname.split('/')[2]
-  const data = blogdata.blogdata.filter(data => data.slug === slug)
+  const filterdata = blogdata.blogdata.filter(data => data.slug === slug)
+  const data = filterdata[0]
   console.log(data)
   return (
     <div className='px-6 md:px-24  flex flex-col gap-y-5 md:gap-y-10 relative pt-24 md:pt-48'>
-      <p className='font-normal text-4xl md:text-7xl max-w-full text-center uppercase mx-auto !leading-tight'>{data[0]?.heading}</p>
+      <p className='font-normal text-4xl md:text-7xl max-w-full text-center uppercase mx-auto !leading-tight'>{data?.heading}</p>
       <div className={`flex items-center px-5 py-2 md:px-10 md:py-3 rounded-full cursor-pointer bg-[#3A3A3A] mx-auto`} style={{ whiteSpace: 'nowrap', minWidth: 'fit-content' }}>
-        <p className='text-sm md:text-lg'>{data[0]?.date}</p>
+        <p className='text-sm md:text-lg'>{data?.date}</p>
       </div>
-      <img src={data[0]?.icon} alt="hero" className='w-full md:w-5/6 mx-auto object-cover md:h-[80vh] rounded-lg md:rounded-2xl' />
-      <p className='text-center font-normal w-full md:w-5/6 mx-auto text-xs md:text-lg opacity-60'>{data[0]?.content}</p>
+      <img src={data?.icon} alt="hero" className='w-full md:w-5/6 mx-auto object-cover md:h-[80vh] rounded-lg md:rounded-2xl' />
+      {/* <p className='text-center font-normal w-full md:w-5/6 mx-auto text-xs md:text-lg opacity-60'>{data?.content}</p> */}
+      {data?.content.map((data, index) => (
+        <Blogstructure key={index} data={data} />
+      ))}
     </div>
   )
 }
--- src/data/blogdata.js
+++ src/data/blogdata.js
@@ -7,18 +7,188 @@ export const blogdata = [
         heading: "Selling Your Products Online in 2023: A Step-by-Step Guide From Choosing Products to Marketing.",
         icon: img1,
         date: "October 11, 2023",
-        slug: "selling-your-products-online-in-2023"
+        slug: "selling-your-products-online-in-2023",
+        content: [
+            {
+                content: "Are you looking to embark on a journey into the world of online business? Whether you're a budding entrepreneur or a seasoned professional, establishing a successful online business involves a strategic approach. In this blog, we'll guide you through the essential steps to set yourself up for success. Let's get started.",
+                type: "content"
+            },
+            {
+                content: "1. Choose the Right Product:",
+                type: "subheading"
+            },
+            {
+                content: "Not all products are created equal when it comes to online sales. To begin, conduct thorough research to identify products with high demand and a promising profit margin. Selecting the right product is the foundation of your online business, and it can significantly impact your success.",
+                type: "content"
+            },
+            {
+                content: "2. Select the Optimal Online Sales Channel:",
+                type: "subheading"
+            },
+            {
+                content: "The next crucial decision is choosing the right online sales channel. Options abound, including popular marketplaces like Amazon and eBay, your very own e-commerce store, and various social media platforms. Your choice should align with your target market, product type, and budget. This decision sets the stage for where and how you'll connect with your audience.",
+                type: "content"
+            },
+            {
+                content: "3. Establish Your Online Store:",
+                type: "subheading"
+            },
+            {
+                content: "If you decide to launch your online store, selecting the right platform is key. Options like Shopify or WooCommerce are user-friendly and customizable. Ensure that your website design is not only visually appealing but also user-friendly. High-quality images and detailed product descriptions are integral to making a strong first impression.",
+                type: "content"
+            },
+            {
+                content: "4. Implement Effective Marketing Strategies:",
+                type: "subheading"
+            },
+            {
+                content: "Your online store is up and running, but now it's time to spread the word. Effective marketing is the bridge connecting your products to potential customers. Explore various marketing approaches, including Search Engine Optimization (SEO) to enhance your online visibility, social media marketing to engage your audience, and paid advertising to reach new prospects. The right marketing strategy will vary based on your unique business and target audience.",
+                type: "content"
+            },
+            {
+                content: "5. Prioritize Exceptional Customer Service:",
+                type: "subheading"
+            },
+            {
+                content: "Outstanding customer service is the bedrock of a successful online business. Being responsive to customer inquiries, addressing issues promptly and fairly, and ensuring timely product deliveries can make all the difference. Building trust and loyalty among your customers will lead to repeat business and word-of-mouth referrals.",
+                type: "content"
+            },
+            {
+                content: "In conclusion, launching and growing a thriving online business demands careful planning and execution. By selecting the right product, sales channel, and marketing strategies while delivering top-notch customer service, you'll position your online venture for long-term success. Remember that each step in this journey is an opportunity to learn, adapt, and refine your approach as you build your online presence and reach your business goals.",
+                type: "content"
+            }
+        ]
     },
     {
         heading: "Unlocking the Power of Lead Generation for Your Business; Your B2B Growth Book",
         icon: img2,
         date: "October 11, 2023",
-        slug: "unlocking-the-power-of-lead-generation-for-your-business"
+        slug: "unlocking-the-power-of-lead-generation-for-your-business",
+        content: [
+            {
+                content: "In the ever-evolving landscape of business, generating leads stands out as a cornerstone of growth. The challenge lies in navigating the multitude of lead generation strategies at your disposal. So, where should you begin, and how can you effectively generate leads for your business? We've got you covered.",
+                type: "content"
+            },
+            {
+                content: "1. Crafting a Showcase with a Portfolio Website:",
+                type: "subheading"
+            },
+            {
+                content: "Your portfolio website is more than just a digital presence; it's your visual calling card. It's your opportunity to display the essence of your business and the remarkable work you do. Make it count by: ",
+                type: "ul",
+                ul: [
+                    "Showcasing Your Best Work: Give your visitors a taste of your capabilities through high-quality images, videos, and client testimonials.",
+                    "SEO Optimization: Elevate your visibility in the online realm by embracing the power of SEO. Here's how:",
+                    "Keywords Matter: Incorporate relevant keywords throughout your website. This helps search engines understand your content and ranks your site higher.",
+                    "Content is King: Craft informative, engaging, and audience-relevant content. Quality content not only keeps visitors on your site but also attracts potential leads.",
+                    "The Power of Backlinks: Build backlinks from reputable sources to bolster your site's authority and enhance its ranking."
+                ]
+            },
+            {
+                content: "2. Leveraging Social Media:",
+                type: "subheading"
+            },
+            {
+                content: "In today's interconnected world, social media is a goldmine for lead generation. Unleash the potential of these platforms by:",
+                type: "ul",
+                ul: [
+                    "Sharing Engaging Content: Offer valuable content on your social media pages, and foster engagement with your audience.",
+                    "Targeted Advertising: Utilize social media advertising to reach specific demographics and interests, ensuring your message reaches the right ears.",
+                ]
+            },
+            {
+                content: "3. In-Person Networking:",
+                type: "subheading"
+            },
+            {
+                content: "Sometimes, the most fruitful opportunities come from face-to-face interactions. Attend industry events and meetups to:",
+                type: "ul",
+                ul: [
+                    "Connect and Network: Forge valuable connections with potential customers and fellow professionals in your field.",
+                    "Be Prepared: Equip yourself with business cards and a compelling pitch that introduces your business.."
+                ]
+            },
+            {
+                content: "4. The Power of Free Trials and Consultations:",
+                type: "subheading"
+            },
+            {
+                content: "An enticing offer can captivate your audience's interest. Provide free consultations or trials to: ",
+                type: "ul",
+                ul: [
+                    " Educate Your Audience: Enable people to explore what your business can offer, nurturing their interest and potentially leading to conversions.",
+                ]
+            },
+            {
+                content: "These are solid strategies for lead generation, but the journey doesn't end here. Here are a few additional tips to boost your lead generation efforts:",
+                type: "ul",
+                ul: [
+                    "CRM Systems: Implement a CRM system to efficiently manage and track leads' interactions with your business, aiding in the conversion process.",
+                    "Email Marketing: Create targeted email marketing campaigns to maintain engagement with leads and guide them toward becoming customers.",
+                    "Paid Advertising: Explore paid advertising campaigns to reach a wider audience; however, be mindful of your budget and track results for optimization."
+                ]
+            },
+            {
+                content: "Remember, lead generation is a continuous process, but it's an indispensable one if you aspire to witness your business thrive. By applying the strategies discussed above, you can establish a robust lead generation framework that fuels your business's growth and success.",
+                type: "content"
+            }
+        ]
     },
     {
         heading: "Discovering the Ideal Tech Co-Founder for Your Product Venture; From Strategizing to Scaling",
         icon: img3,
         date: "October 11, 2023",
-        slug: "discovering-the-ideal-tech-co-founder-for-your-product-venture"
+        slug: "discovering-the-ideal-tech-co-founder-for-your-product-venture",
+        content: [
+            {
+                content: "In today's ever-evolving tech landscape, your journey as an entrepreneur is marked by an unending quest for innovation and a relentless pursuit of success. Procube Product Labs is more than just a technology company; it's your steadfast ally on this exciting journey. With a strong commitment to product development and a holistic approach to your business's growth, we're here to guide you from inception to thriving reality. Let's dive deeper into how we can empower your search for a tech founder and help you strategize your vision from branding to marketing.",
+                type: "content"
+            },
+            {
+                content: "Access to Top Tech Talent:",
+                type: "subheading"
+            },
+            {
+                content: "In the digital age, tech talent is the lifeblood of your product company. Procube Product Labs connects you with a diverse and highly skilled network of tech professionals. Whether your project requires mobile app development, web development, or custom software solutions, we've got the experts to match your specific needs. Our talent pool is driven not just by skills but by a profound passion for bringing your product ideas to fruition.",
+                type: "content"
+            },
+            {
+                content: "Mentorship and Guidance:",
+                type: "subheading"
+            },
+            {
+                content: "While technology is at the core of our expertise, we recognize that a successful product journey encompasses much more. We offer mentorship and guidance to help you navigate the intricate world of product development. Our seasoned experts serve as your trusted advisors, providing insights into technology choices, business strategies, and market dynamics. They are committed to your success and are by your side every step of the way.",
+                type: "content"
+            },
+            {
+                content: "Product Development Expertise:",
+                type: "subheading"
+            },
+            {
+                content: "Creating a successful product involves a harmonious blend of technology, design, branding, and marketing. Procube Product Labs excels in each of these domains:",
+                type: "ul",
+                ul: [
+                    "Web and Mobile App Development: Our team specializes in crafting responsive and user-friendly web and mobile applications, ensuring your product reaches your audience on various platforms.",
+                    "UI/UX Design: A seamless and intuitive user experience is at the heart of any successful product. Our designers ensure that your product isn't just functional but also aesthetically pleasing and user-friendly.",
+                    "Branding: Building a strong brand identity is paramount. We work with you to create a brand that not only resonates with your audience but also sets you apart in the market.",
+                    "Digital Marketing: A product's reach is vital to its success. Our digital marketing experts employ strategies that drive user engagement and conversions, making your product a commercial triumph.",
+                ]
+            },
+            {
+                content: "Strategizing from Branding to Marketing:",
+                type: "subheading"
+            },
+            {
+                content: "At Procube Product Labs, we understand that a great product alone isn't enough; it needs the right strategy to reach its potential. We offer end-to-end services, from strategizing your brand's identity to devising effective marketing campaigns. Whether you're looking to define your brand's unique identity or launch a powerful marketing campaign, we've got the expertise to transform your vision into a reality. ",
+                type: "content"
+            },
+            {
+                content: "Your journey to find the perfect tech founder and create a successful product company is a transformative adventure. With Procube Product Labs as your guiding light, you have access to the resources, expertise, and network needed to realize your vision. Don't let your dream product remain a dream ",
+                type: "ul",
+                ul : [
+                    "take the first step with Procube Product Labs and turn your product company into a thriving success story. Your path to innovation and strategic success begins here, and we're here to walk it with you. Join us in the pursuit of excellence and watch your vision come to life."
+                ]
+            }
+        ]
     }
 ];
