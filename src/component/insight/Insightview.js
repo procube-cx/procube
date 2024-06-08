0620a69d5af6c640addcf35615f2aa75c3a15703
dhanush <dhanushpk50@gmail.com>
2024-06-08 21:00:22 +0530

    changes

--- src/component/insight/Insightview.js
+++ src/component/insight/Insightview.js
@@ -10,7 +10,7 @@ const Blogstructure = ({ data }) => {
         <>
           <p className='text-sm md:text-lg opacity-60 !leading-relaxed'>{data.content}</p>
           <ul className=''>{data.ul.map((data, index) => (
-            <li key={index} style={{paddingLeft : '10px'}}>
+            <li key={index} style={{ paddingLeft: '10px' }}>
               <p className='text-sm md:text-lg opacity-60 !leading-relaxed '>- {data}</p>
             </li>
           ))}</ul>
@@ -26,18 +26,26 @@ const Insightview = (blogdata) => {
   const filterdata = blogdata.blogdata.filter(data => data.slug === slug)
   const data = filterdata[0]
   return (
-    <div className='px-6 md:px-24  flex flex-col gap-y-5 md:gap-y-10 relative pt-24 md:pt-48'>
-      <p className='font-normal text-4xl md:text-7xl max-w-full text-center uppercase mx-auto !leading-tight'>{data?.heading}</p>
-      <div className={`flex items-center px-5 py-2 md:px-10 md:py-3 rounded-full cursor-pointer bg-[#3A3A3A] mx-auto`} style={{ whiteSpace: 'nowrap', minWidth: 'fit-content' }}>
-        <p className='text-sm md:text-lg'>{data?.date}</p>
-      </div>
-      <img src={data?.icon} alt="hero" className='w-full md:w-5/6 mx-auto object-cover md:h-[80vh] rounded-lg md:rounded-2xl' />
-      <div className='w-full md:w-5/6 mx-auto'>
-        {data?.content.map((data, index) => (
-          <Blogstructure key={index} data={data} />
-        ))}
-      </div>
-    </div>
+    <>
+      {data === undefined ?
+        (<div className='px-6 md:px-24  flex flex-col gap-y-5 md:gap-y-10 relative pt-24 md:pt-48'>
+          <p className='font-normal text-4xl max-w-full uppercase !leading-tight'>BLOG NOT FOUND</p>
+        </div>)
+        :
+        (<div className='px-6 md:px-24  flex flex-col gap-y-5 md:gap-y-10 relative pt-24 md:pt-48'>
+          <p className='font-normal text-4xl md:text-7xl max-w-full text-center uppercase mx-auto !leading-tight'>{data?.heading}</p>
+          <div className={`flex items-center px-5 py-2 md:px-10 md:py-3 rounded-full cursor-pointer bg-[#3A3A3A] mx-auto`} style={{ whiteSpace: 'nowrap', minWidth: 'fit-content' }}>
+            <p className='text-sm md:text-lg'>{data?.date}</p>
+          </div>
+          <img src={data?.icon} alt="hero" className='w-full md:w-5/6 mx-auto object-cover md:h-[80vh] rounded-lg md:rounded-2xl' />
+          <div className='w-full md:w-5/6 mx-auto'>
+            {data?.content.map((data, index) => (
+              <Blogstructure key={index} data={data} />
+            ))}
+          </div>
+        </div>)
+      }
+    </>
   )
 }
 
