a51fae66c7ccdaa55957d5a9b49eb8b623a12b09
dhanush <dhanushpk50@gmail.com>
2024-06-08 15:54:29 +0530

    changes

--- src/component/hero/Footer.js
+++ src/component/hero/Footer.js
@@ -65,7 +65,7 @@ const data = {
         },
         {
             title: 'Instagram',
-            links: ''
+            links: 'https://www.instagram.com/procube.cx/'
         },
         {
             title: 'Dribbble',
@@ -73,7 +73,7 @@ const data = {
         },
         {
             title: 'Linkedin',
-            links: ''
+            links: 'https://www.linkedin.com/company/procubecx/'
         }
     ]
 };
@@ -84,10 +84,10 @@ const Footer = () => {
         <div className='flex flex-col w-full  px-6 md:px-24 pt-12 md:pt-32 bg-black rounded-t-[30px] md:rounded-t-[60px]'>
             <div className='flex flex-col w-full items-center gap-5 md:gap-14 mb-16 md:mb-36'>
                 <div className='font-normal text-4xl md:text-8xl !leading-[45px] md:!leading-[120px] max-w-4xl text-center uppercase'>Let’s turn your idea into reality</div>
-                <div className='button-hover bg-[#7605C1] px-12 md:px-16 py-3 md:py-4 text-base md:text-4xl rounded-full cursor-pointer'>Get Started</div>
+                <a href='https://98zehvobtv4.typeform.com/to/dHNwncmO' className='button-hover bg-[#7605C1] px-12 md:px-16 py-3 md:py-4 text-base md:text-4xl rounded-full cursor-pointer'>Get Started</a>
             </div>
             <div className='flex flex-col md:flex-row mb-10  md:mb-20'>
-                <div className='hidden md:flex border px-5 py-10 md:px-7 md:py-16 rounded-full h-auto md:h-44 w-16 md:w-auto my-8 md:my-0 mr-16 md:mr-10 cursor-pointer' onClick={() => window.scrollTo({top:0,behavior:'smooth'})}>
+                <div className='hidden md:flex border px-5 py-10 md:px-7 md:py-16 rounded-full h-auto md:h-44 w-16 md:w-auto my-8 md:my-0 mr-16 md:mr-10 cursor-pointer' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                     <img src={arrow} alt='arrow' className='w-6 md:w-8  ' />
                 </div>
                 <div className='flex-1 my-auto'>
@@ -95,7 +95,9 @@ const Footer = () => {
                         <div className='flex flex-wrap justify-between w-full md:w-3/4'>
                             {data.option4.map((item, index) => {
                                 return (
-                                    <a key={index} href={item.links} className='text-sm md:text-xl hover:opacity-40'>{item.title}</a>
+                                    <a key={index} href={item.links} target="_blank" rel="noopener noreferrer" className='text-sm md:text-xl hover:opacity-40'>
+                                        {item.title}
+                                    </a>
                                 )
                             }
                             )}
@@ -113,7 +115,7 @@ const Footer = () => {
                         }
                         )}
                     </div> */}
-                    
+
                     <div className='flex flex-col col-span-1 gap-8'>
                         {data.option2.map((item, index) => {
                             return (
