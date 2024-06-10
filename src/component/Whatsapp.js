dcedaabe94ce179543f57888ed276a708941db40
dhanush <dhanushpk50@gmail.com>
2024-06-10 13:17:28 +0530

    content change

--- src/component/Whatsapp.js
+++ src/component/Whatsapp.js
@@ -4,7 +4,7 @@ import whatsapp_icon from '../assets/images/whatsapp.svg'
 const Whatsapp = () => {
   return (
     <div className='fixed bottom-5 md:bottom-10 md:right-10 right-5'>
-        <a href='https://wa.me/923000000000' target='_blank' rel='noreferrer' >
+        <a href='https://wa.me/+971503533460' target='_blank' rel='noreferrer' >
             <img src={whatsapp_icon} alt="whatsapp" className='w-14 h-14 md:w-20 md:h-20' />
         </a>
     </div>
